e114ba547f873ac3f215ccd21e1a283a8efd1203
juhi16792 <lrjuheeshwari16@gmail.com>
2017-06-18 17:23:34 -0700

    TODO (1) done

--- S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -42,22 +42,6 @@ public class MainActivity extends AppCompatActivity {
          * real weather data. For now, we want to get something on the screen as quickly as
          * possible, so we'll display this dummy data.
          */
-       /* String[] dummyWeatherData = {
-                "Today, May 17 - Clear - 17°C / 15°C",
-                "Tomorrow - Cloudy - 19°C / 15°C",
-                "Thursday - Rainy- 30°C / 11°C",
-                "Friday - Thunderstorms - 21°C / 9°C",
-                "Saturday - Thunderstorms - 16°C / 7°C",
-                "Sunday - Rainy - 16°C / 8°C",
-                "Monday - Partly Cloudy - 15°C / 10°C",
-                "Tue, May 24 - Meatballs - 16°C / 18°C",
-                "Wed, May 25 - Cloudy - 19°C / 15°C",
-                "Thu, May 26 - Stormy - 30°C / 11°C",
-                "Fri, May 27 - Hurricane - 21°C / 9°C",
-                "Sat, May 28 - Meteors - 16°C / 7°C",
-                "Sun, May 29 - Apocalypse - 16°C / 8°C",
-                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
-        };*/
 
         // TODO (3) Delete the for loop that populates the TextView with dummy data --> Completed
         /*
@@ -65,24 +49,31 @@ public class MainActivity extends AppCompatActivity {
          * the "\n\n\n" after the String is to give visual separation between each String in the
          * TextView. Later, we'll learn about a better way to display lists of data.
          */
-        /*for (String dummyWeatherDay : dummyWeatherData) {
-            mWeatherTextView.append(dummyWeatherDay + "\n\n\n");
-        }*/
-
         // TODO (9) Call loadWeatherData to perform the network request to get the weather
     }
 
     // TODO (8) Create a method that will get the user's preferred location and execute your new AsyncTask and call it loadWeatherData
 
+
     // TODO (5) Create a class that extends AsyncTask to perform network requests --> Completed
     public class NetworkRequests extends AsyncTask<String, Void, String[]> {
+
         // TODO (6) Override the doInBackground method to perform your network requests --> Completed
         @Override
         protected String[] doInBackground(String... params) {
+
+            //tasks to do here
+            // 1. this process should work in background and get the weather data from the URL built from TODO (1)
+            // 2. before starting this task complete TOOD (1) which is NetworkUtils
+            // 3. params[0] should have the location
+            // 4. URL should be built with the location specified
+            // 5. And then pass the JSON
+            // 6. if params[0] is null then just return null
             return null;
         }
-        // TODO (7) Override the onPostExecute method to display the results of the network request -->Completed
 
+        // TODO (7) Override the onPostExecute method to display the results of the network request
+        // -->Completed
         @Override
         protected void onPostExecute(String[] weatherData){
             for(String data: weatherData){
@@ -90,7 +81,4 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
-
-    // TODO (7) Override the onPostExecute method to display the results of the network request
 }
